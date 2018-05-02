70b8741e51a3733d53daf116ad56ed03b56f0980
dcronkite <dcronkite@gmail.com>
2018-05-02 14:11:34 -0700

    Added 'to' to depth pattern (continued to 8cm...)

--- src/colonoscopy_algo/extract/patterns.py
+++ src/colonoscopy_algo/extract/patterns.py
@@ -3,5 +3,5 @@ import re
 NUMBER_PATTERN = re.compile(r'(\d{1,3})', re.IGNORECASE)
 DEPTH_PATTERN = re.compile(r'(\d{1,3})\W*[cm]m', re.IGNORECASE)
 SIZE_PATTERN = re.compile(r'(<?\d{1,3}(?:\.\d)?)\W*[cm]m', re.IGNORECASE)
-AT_DEPTH_PATTERN = re.compile(r'(?:at|@)\W*(\d{1,3}(?:\.\d)?)\W*[cm]m', re.IGNORECASE)
+AT_DEPTH_PATTERN = re.compile(r'(?:at|@|to)\W*(\d{1,3}(?:\.\d)?)\W*[cm]m', re.IGNORECASE)
 CM_DEPTH_PATTERN = re.compile(r'(\d{2,3})\W*cm', re.IGNORECASE)
