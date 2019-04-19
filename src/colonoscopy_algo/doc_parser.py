2e6437a8e117c4b5f253ba371a4434e63bdeb547
dcronkite <dcronkite@gmail.com>
2019-04-19 10:01:24 -0700

    Cleaning extra ocr junk at the end of lines

--- src/colonoscopy_algo/doc_parser.py
+++ src/colonoscopy_algo/doc_parser.py
@@ -1,4 +1,82 @@
 import re
+import string
+
+
+def remove_ocr_junk(line):
+    tokens = line.split()
+    up_letters = re.compile(f'[{string.ascii_uppercase}]')
+    lw_letters = re.compile(f'[{string.ascii_lowercase}]')
+    exp_punct = re.compile(r'[.,]')
+    numbers = re.compile(r'[0-9]')
+    # number of letters/numbers with unexpected punctuation
+    # length
+    # language model n-grams
+    scores = []  # higher score -> junk
+    for token in reversed(tokens):
+        ltoken = token.lower()
+        size = len(token)
+        up_let = len(up_letters.findall(token))
+        lw_let = len(lw_letters.findall(token))
+        punct = 1 if token[-1] in ['.,'] else 0
+        nums = len(numbers.findall(token))
+        rest = size - up_let - lw_let - nums - punct
+        if size > 4:
+            if size - lw_let - up_let - punct == 0 and up_let <= 1:
+                break
+            elif nums >= size - 1:
+                break
+            elif size > 5 and lw_let + up_let + punct >= size - 1:
+                break
+            elif size > 6 and lw_let + up_let + punct >= size - 2:
+                break
+            else:
+                # TODO: check if is word
+                # TODO: check if removing last letters creates a word
+                scores.append(10)
+        elif size == 1:
+            if nums == 1:
+                scores.append(0)
+            elif token == 'a':
+                scores.append(0)
+            elif token == 'A':
+                scores.append(2)
+            else:
+                scores.append(10)
+        elif size == 2:
+            if nums == 2:
+                scores.append(0)
+            elif nums == 1 and (up_let + lw_let == 1 or punct):
+                scores.append(2)
+            elif ltoken in ['in', 'an', 'on', 'or', 'no', 'it', 'to', 'by', 'of']:
+                scores.append(1)
+            else:
+                scores.append(10)
+        elif size == 3:
+            if nums == 3:
+                scores.append(0)
+            elif ltoken in ['its', 'the', 'and', 'was']:
+                scores.append(0)
+            else:
+                scores.append(10)
+        elif size == 4:
+            if nums in [3, 4]:
+                scores.append(3)
+            elif lw_let + punct + up_let == size and up_let <= 1:
+                scores.append(0)
+            elif lw_let == 0:
+                scores.append(7)
+            elif lw_let == size - 1:
+                scores.append(3)
+            else:
+                scores.append(10)
+    line_end = None
+    for i, score in enumerate(scores):
+        if score >= 5:
+            line_end = i
+    if line_end is None:
+        return line
+    else:
+        return ' '.join(tokens[:-(line_end + 1)])
 
 
 def parse_file(text):
@@ -21,6 +99,7 @@ def parse_file(text):
     skip_mode = True
     has_page = bool(page_pat.search(text))
     for line in text.split('\n'):
+        line = remove_ocr_junk(line)
         # find procedure/findings
         if not line:
             continue
