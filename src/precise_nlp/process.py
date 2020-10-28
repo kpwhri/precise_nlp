f067987c1daeb812ab955ae4a2edbb711fc34771
dcronkite <dcronkite@gmail.com>
2020-10-27 19:22:30 -0700

    Add 'tsv' as synonym of 'tab'

--- src/precise_nlp/process.py
+++ src/precise_nlp/process.py
@@ -222,7 +222,7 @@ def get_data(filetype, path, identifier=None, path_text=None, cspy_text=None, en
             df = filetype
         elif filetype == 'csv':
             df = pd.read_csv(path, encoding=encoding)
-        elif filetype == 'tab':
+        elif filetype == 'tab' or filetype == 'tsv':
             df = pd.read_csv(path, sep='\t', encoding=encoding)
         elif filetype == 'sas':
             df = pd.read_sas(path, encoding=encoding)
