b858709b7176e2a464db0c94e6f129dafe473b96
dcronkite <dcronkite@gmail.com>
2021-11-30 13:55:45 -0800

    Changed dysplasia to account for polarity

--- tests/test_dysplasia.py
+++ tests/test_dysplasia.py
@@ -1,3 +1,5 @@
+import pytest
+
 from precise_nlp.extract.path import JarManager
 
 
@@ -5,4 +7,19 @@ def test_dysplasia_negation():
     text = 'negative for high grade dysplasia'
     jm = JarManager()
     jm.cursory_diagnosis_examination(text)
-    assert jm.has_dysplasia() == 0
+    assert jm.has_dysplasia() is False
+
+
+@pytest.mark.parametrize('text, exp', [
+    ('negative for high grade dysplasia', 0),
+    ('high grade dysplasia', 1),
+    ('', 99),
+    ('no evidence of highgrade dysplasia', 0),
+    ('evidence of highgrade dysplasia', 1),
+    ('low grade dysplasia', 0),
+    ('A. lots of highgrade dysplasia; B. negative for highgrade dysplasia', 1),
+])
+def test_get_any_dysplasia(text, exp):
+    jm = JarManager()
+    jm.cursory_diagnosis_examination(text)
+    assert jm.get_any_dysplasia() == exp
