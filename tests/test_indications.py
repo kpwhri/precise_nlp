1980b4d7a7fda2e7c228f30f457403d1b78d16d7
dcronkite <dcronkite@gmail.com>
2021-12-07 11:13:09 -0800

    Cleaned up 'follow-up' regex

--- tests/test_indications.py
+++ tests/test_indications.py
@@ -47,6 +47,7 @@ def test_indication_section(text, exp):
     ('Indications: Ulcerative Proctitis: 556.2 - K51.20\nOccult Blood: 792.1 - R19.5', Indication.DIAGNOSTIC),
     ('Indications: Ulcerative proctitis: 556.2 - K51.20\nOccult Blood: 792.1 - R19.5', Indication.DIAGNOSTIC),
     ('Indications: Personal', Indication.UNKNOWN),
+    ('Indications: Follow-up cancer', Indication.SURVEILLANCE),
 ])
 def test_indications(text, exp):
     assert CspyManager(text).get_indication() == exp
