f854588432c698d6b44a72fbd2c1fdecad1194c2
dcronkite <dcronkite@gmail.com>
2021-12-07 12:12:21 -0800

    Added patterns for incomplete procedure extent

--- tests/test_extent.py
+++ tests/test_extent.py
@@ -10,6 +10,14 @@ from precise_nlp.extract.utils import Extent
     ('visualized the ileocecal valve', Extent.COMPLETE),
     ('visualized the appendiceal orifice', Extent.COMPLETE),
     ('ileum is seen', Extent.COMPLETE),
+    ('extent of the procedure', Extent.INCOMPLETE),
+    ('extent of the procedure: descending colon', Extent.INCOMPLETE),
+    ('terminal ileum not reached', Extent.INCOMPLETE),
+    ('ileocecal valve not visualised', Extent.INCOMPLETE),
+    ('unable to see the cecum', Extent.INCOMPLETE),
+    ('ileum is not seen', Extent.INCOMPLETE),
+    ('did not visualize the ileocecal valve', Extent.INCOMPLETE),
+    ('did not visualize the appendiceal orifice', Extent.INCOMPLETE),
 ])
 def test_extent(text, exp):
     assert CspyManager(text).get_extent() == exp
