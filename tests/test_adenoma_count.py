c8a26e59907b4fe9e4b57b4983c3ce9860ba2aa0
dcronkite <dcronkite@gmail.com>
2021-12-13 15:29:52 -0800

    Added sessile serrated tests

--- tests/test_adenoma_count.py
+++ tests/test_adenoma_count.py
@@ -18,6 +18,9 @@ C. COLON, DESCENDING POLYP, POLYPECTOMY:
     ('A. COLON, TRANSVERSE POLYPS, POLYPECTOMY:\n- Tubular adenoma, | fragment.', 1),
     ('POLYPS: adenoma', 1),
     ('A. COLON, TRANSVERSE POLYPS, POLYPECTOMY:\n- Tubular adenoma, 1 of 5 fragments.', 1),
+    ('sessile serrated adenoma', 0),
+    ('sessile serrated lesion', 0),
+    ('sessile serrated polyp', 0),
 ])
 def test_adenoma_count(text, exp):
     assert PathManager(text).get_adenoma_count().count == exp
