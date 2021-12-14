77d9f3448bb9ab8fd3856308a600ad4272cdbc18
dcronkite <dcronkite@gmail.com>
2021-12-14 12:25:06 -0800

    Passing first test for identifying findings with a pattern

---
+++ tests/test_finidng_patterns.py
@@ -0,0 +1,14 @@
+import pytest
+
+from precise_nlp.extract.cspy import CspyManager
+from precise_nlp.extract.cspy.finding_patterns import apply_finding_patterns
+
+
+@pytest.mark.parametrize('text, exp_count, exp_size, exp_locations', [
+    ('Polyp (15 mm) in the descending colon', 1, 15, {'descending'})
+])
+def test_finding_pattern(text, exp_count, exp_size, exp_locations):
+    findings = list(apply_finding_patterns(text))
+    assert findings[0].count == exp_count
+    assert findings[0].size == exp_size
+    assert set(findings[0].locations) == set(exp_locations)
