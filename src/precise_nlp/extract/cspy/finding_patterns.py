f6bf7d8e54d008cb121943285dd24dc4ab5208f9
dcronkite <dcronkite@gmail.com>
2021-12-15 09:43:37 -0800

    Added additional tests; collect information based on location in colonscopy report of finding

--- src/precise_nlp/extract/cspy/finding_patterns.py
+++ src/precise_nlp/extract/cspy/finding_patterns.py
@@ -2,19 +2,28 @@ from typing import List
 
 from regexify.pattern import Pattern
 
-from precise_nlp.extract.cspy.finding_builder import Finding
+from precise_nlp.extract.cspy.finding_builder import Finding, FindingSource
+
+colon = '(colon|flexure)'
 
 FINDING_PATTERNS = {
-    'POLYP_SIZE_IN_LOCATION': Pattern(r'polyp (?P<size>\d+) mm in the (?P<location>\w+) colon'),
-    'POLYP_SIZE_3W_LOCATION': Pattern(r'polyp (?P<size>\d+) mm (\w+ ){0,3}(?P<location>\w+) colon'),
+    'POLYP_SIZE_IN_LOCATION': Pattern(rf'polyp (?P<size>\d+) mm in the (?P<location>\w+) {colon}'),
+    'POLYP_SIZE_3W_LOCATION': Pattern(rf'polyp (?P<size>\d+) mm (\w+ ){{0,3}}(?P<location>\w+) {colon}'),
 }
 
 
-def apply_finding_patterns(text) -> List[Finding]:
+def apply_finding_patterns(text, source: FindingSource = None) -> List[Finding]:
     for name, pat in FINDING_PATTERNS.items():
         if m := pat.matches(text):
-            yield Finding(
-                count=1,
-                sizes=(int(m.group('size')),),
-                locations=(m.group('location'),),
-            )
+            d = m.groupdict()
+            match list(d.keys()):
+                case ['size', 'location']:
+                    yield Finding(
+                        count=1,
+                        sizes=(int(m.group('size')),),
+                        locations=(m.group('location'),),
+                        source=source,
+                    )
+                case other:
+                    raise ValueError(f'Unrecognized: {other}')
+            break
--- tests/test_finding_patterns.py
+++ tests/test_finding_patterns.py
@@ -7,7 +7,8 @@ from precise_nlp.extract.cspy.finding_patterns import apply_finding_patterns
 @pytest.mark.parametrize('text, exp_count, exp_size, exp_locations', [
     ('Polyp (15 mm) in the descending colon', 1, 15, {'descending'}),
     ('Polyp (10 mm)-in the sigmoid colon. (Polypectomy)', 1, 10, {'sigmoid'}),
-    ('Polyp (10 mm)-in the distal sigmoid colon. (Polypectomy)', 1, 10, {'sigmoid'})
+    ('Polyp (10 mm)-in the distal sigmoid colon. (Polypectomy)', 1, 10, {'sigmoid'}),
+    ('Polyp (12 mm) in the splenic flexure. (Polypectomy)', 1, 12, {'splenic'})
 ])
 def test_finding_pattern(text, exp_count, exp_size, exp_locations):
     findings = list(apply_finding_patterns(text))
