6e8359ede1687674770a84b2fba633c953663eba
dcronkite <dcronkite@gmail.com>
2021-12-20 10:25:09 -0800

    Updated regex to allow for 'pedunculated'

--- src/precise_nlp/extract/cspy/finding_patterns.py
+++ src/precise_nlp/extract/cspy/finding_patterns.py
@@ -45,7 +45,7 @@ FINDING_PATTERNS = {
         rf'polyp location {_location_all()} size {_size_qual()}'
     ),
     f'LOCATION_SIZE_POLYP': Pattern(
-        rf'{_location_all()} {_count()} {_size_qual()} (sessile )?polyp'
+        rf'{_location_all()} {_count()} {_size_qual()} (?:(?:sessile|pedunc\w+) )?polyp'
     )
 }
 
